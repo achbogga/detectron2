b46c74b8b1a272b0fc386b64b735d436ffb12348
aboggaram@michaelangelo <aboggaram@iunu.com>
2022-10-31 18:27:33 -0400

    adding the lazy config training script

---
+++ train_octiva.sh
@@ -0,0 +1,1 @@
+time python3 tools/lazyconfig_train_net.py --config-file configs/new_baselines/iunu_mask_rcnn_R_50_FPN_100ep_LSJ.py --num-gpus 2 | tee /home/aboggaram/logs/octiva_training_945_images.log
